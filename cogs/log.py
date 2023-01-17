60d08c69bc382ad7bf461247ee90da791b2755ef
jamesbt365 <jamesbt365@gmail.com>
2023-01-17 20:04:30 +0000

    feat: add blacklisted word checking

--- cogs/log.py
+++ cogs/log.py
@@ -7,17 +7,33 @@ from colorama import init, Fore, Style
 
 colorama.init(autoreset=True)
 
+badlist = []
+with open("./cogs/words/badwords.txt") as f:
+    for line in f:
+        badlist.append(line.strip())
+
+fixlist = []
+with open("./cogs/words/fixwords.txt") as f:
+    for line in f:
+        fixlist.append(line.strip())
 class Log(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
 
-# TODO: catch blacklisted words
 # TODO: log most other events.
 
   @commands.Cog.listener()
   async def on_message(self, message):
-    print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {message.content}")
+    message_words = message.content.lower().split(" ")
+    bad_words = set(badlist).intersection(message_words)
+    fixed_words = set(fixlist).intersection(bad_words)
+    if bad_words - fixed_words:
+        print(f"Flagged for bad word(s): {Style.BRIGHT}{Fore.RED}{bad_words - fixed_words}")
+        print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {Style.BRIGHT}{Fore.RED}{message.content}")
+    else:
+        print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {message.content}")
+            
 
   @commands.Cog.listener()
   async def on_message_delete(self, message):
