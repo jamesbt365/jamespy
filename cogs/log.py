ea9cd315ed21345714cf09d9677ed246028c8321
jamesbt365 <jamesbt365@gmail.com>
2023-02-11 23:30:03 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- cogs/log.py
+++ cogs/log.py
@@ -2,63 +2,67 @@ import asyncio
 import discord
 from discord.ext import commands
 from discord import Embed
-import re
 
 import colorama
 from colorama import init, Fore, Style
 
 colorama.init(autoreset=True)
 
-badlist = []
-with open("./cogs/words/badwords.txt") as f:
-    for line in f:
-        badlist.append(line.strip())
 
-fixlist = []
-with open("./cogs/words/fixwords.txt") as f:
-    for line in f:
-        fixlist.append(line.strip())
+def read_words_from_file(file_path):
+    words = []
+    with open(file_path) as f:
+        for line in f:
+            words.append(line.strip())
+    return words
 
-cameralist = []
-with open("./cogs/words/cameras.txt") as f:
-    for line in f:
-        cameralist.append(line.strip())
+
+badlist = read_words_from_file("./cogs/words/badwords.txt")
+fixlist = read_words_from_file("./cogs/words/fixwords.txt")
+cameralist = read_words_from_file("./cogs/words/cameras.txt")
+
+
 class Log(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-  def __init__(self, bot):
-    self.bot = bot
+    # TODO: log most other events.
 
-# TODO: log most other events.
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        messagewords = message.content.lower().split(" ")
+        blacklisted_words = [word for word in messagewords if any(j in word and word not in fixlist for j in badlist)]
+        if blacklisted_words:
+            print(f"Flagged for bad word(s): {Style.BRIGHT}{Fore.RED}" + ", ".join(blacklisted_words))
+            print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {Style.BRIGHT}{Fore.RED}{message.content}")
+        else:
+            print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {message.content}")
 
-  @commands.Cog.listener()
-  async def on_message(self, message):
-    messagewords = message.content.lower().split(" ")
-    blacklisted_words = [word for word in messagewords if any(j in word and word not in fixlist for j in badlist)]
-    if blacklisted_words:
-        print(f"Flagged for bad word(s): {Style.BRIGHT}{Fore.RED}" + ", ".join(blacklisted_words))
-        print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {Style.BRIGHT}{Fore.RED}{message.content}")
-    else:
-        print(f"{Fore.LIGHTBLACK_EX}[{message.guild}] [#{message.channel}]{Fore.RESET} {message.author}: {message.content}")
-    if message.author.id != self.bot.user.id:
-      if any(word in message.content.lower() for word in cameralist):
-        embed = Embed(title=f"Server: {message.guild}", description=f"A message sent in **#{message.channel.name}** by **{message.author}**: {message.content}", color=0x00ff00)
-        await self.bot.get_user(326444255361105920).send(embed=embed)
+        if message.author.id != self.bot.user.id:
+            if any(word in message.content.lower() for word in cameralist):
+                embed = Embed(
+                    title=f"Server: {message.guild}",
+                    description=f"A message sent in **#{message.channel.name}** by **{message.author}**: {message.content}",
+                    color=0x00FF00)
+                await self.bot.get_user(326444255361105920).send(embed=embed)
 
-  @commands.Cog.listener()
-  async def on_message_delete(self, message):
-    print(f"{Style.DIM}{Fore.LIGHTRED_EX}[{message.guild}] [#{message.channel}] A message from {Style.RESET_ALL}{message.author}{Style.DIM}{Fore.LIGHTRED_EX} was deleted: {message.content}")
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        print(
+            f"{Style.DIM}{Fore.LIGHTRED_EX}[{message.guild}] [#{message.channel}] A message from {Style.RESET_ALL}{message.author}{Style.DIM}{Fore.LIGHTRED_EX} was deleted: {message.content}"
+        )
 
-  @commands.Cog.listener()
-  async def on_message_edit(self, before, after):
-    if before.content == after.content:
-        return
-    if before.author.id == 250717919221252117: # ignore oink because bombing spams the log.
-      return
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if before.content == after.content:
+          return
+        if (before.author.id == 250717919221252117):  # ignore oink because bombing spams the log.
+          return
 
-    # TODO: ignore oink bot.
-    print(f"{Fore.CYAN}[{before.guild}] [#{before.channel}] A message from {Fore.RESET}{before.author}{Fore.CYAN} was edited:")
-    print(f"{Fore.CYAN}BEFORE: {before.author}: {before.content}")
-    print(f"{Fore.CYAN}AFTER:  {after.author}: {after.content}")
+        print(f"{Fore.CYAN}[{before.guild}] [#{before.channel}] A message from {Fore.RESET}{before.author}{Fore.CYAN} was edited:")
+        print(f"{Fore.CYAN}BEFORE: {before.author}: {before.content}")
+        print(f"{Fore.CYAN}AFTER:  {after.author}: {after.content}")
+
 
 async def setup(bot):
-  await bot.add_cog(Log(bot))
+    await bot.add_cog(Log(bot))
--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import discord
 from discord.ext import commands
 import os
 
-import colorama
-from colorama import init, Fore, Style
-
-colorama.init(autoreset=True)
-
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-
 bot = commands.Bot(command_prefix="-", intents=intents)
 
 async def load_extensions():
@@ -23,6 +16,7 @@ async def load_extensions():
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
 
 async def main():
     async with bot:
